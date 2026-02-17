f483cbe1decca0492a143d85dcfe577385fa78d8
Geordan Gesink <georgegeordan@gmail.com>
2026-02-17 04:21:57 +0100

    add apply update flow

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const Hyperdrive = require('hyperdrive')
 const Corestore = require('corestore')
 const path = require('bare-path')
 const fs = require('bare-fs')
+const fsx = require('fs-native-extensions')
 const ReadyResource = require('ready-resource')
 const plink = require('pear-link')
 const hid = require('hypercore-id-encoding')
 const Localdrive = require('localdrive')
+const { isIOS, isAndroid } = require('which-runtime')
 
 module.exports = class PearRuntime extends ReadyResource {
   constructor(config) {
@@ -17,13 +19,13 @@ module.exports = class PearRuntime extends ReadyResource {
     const { drive: upgrade } = plink.parse(config.upgrade)
     this.dir = config.dir
     this.version = config.version || 0
-    this.app = config.app
-    this.name = config.name
+    this.app = path.join(this.dir, 'pear-runtime/upgrade', isIOS ? 'runtime.ios.bundle' : 'runtime.android.bundle')
+    this.name = isIOS ? 'runtime.ios.bundle' : 'runtime.android.bundle'
     this.key = hid.decode(upgrade.key)
     this.length = upgrade.length || 0
     this.fork = upgrade.fork || 0
     this.link = plink.serialize({ drive: { fork: this.fork, length: this.length, key: this.key }})
-    this.store = config.store || new Corestore(path.join(this.dir, 'corestore'))
+    this.store = config.store || new Corestore(path.join(this.dir, 'pear-runtime/corestore'))
     this.drive = new Hyperdrive(this.store, this.key)
     this.swarm = config.swarm || null
     this.next = null
@@ -37,15 +39,19 @@ module.exports = class PearRuntime extends ReadyResource {
 
   async _open() {
     await this.drive.ready()
-    await fs.mkdir(path.join(this.dir, 'pear-runtime', 'upgrade'), { recursive: true, force: true })
+    await fs.rm(path.join(this.dir, 'pear-runtime/next'), {
+        recursive: true,
+        force: true
+      })
+    await fs.mkdir(path.join(this.dir, 'pear-runtime/next'), { recursive: true, force: true })
     if (!this.swarm) {
       const keyPair = await this.store.createKeyPair('pear-container')
       this.swarm = new Hyperswarm({ keyPair })
     }
-
+    
     this.swarm.on('connection', (connection) => this.store.replicate(connection))
     this.swarm.join(this.drive.core.discoveryKey, { client: true, server: false })
-
+    
     this._updateBackground()
     this.drive.core.on('append', () => this._updateBackground())
   }
@@ -57,6 +63,18 @@ module.exports = class PearRuntime extends ReadyResource {
     await this.swarm.destroy()
   }
 
+  async applyUpdate() {
+    if (!this.updated || this.applied || !this.dir) return
+    this.applied = true
+    if (!await fs.exists(this.app)) {
+      await fs.mkdir(path.join(this.dir, 'pear-runtime/upgrade'), { recursive: true, force: true })
+      await fs.writeFile(this.app, '')
+    }
+
+    await fsx.swap(path.join(this.next, this.name), this.app)
+    await fs.promises.rm(this.next, { recursive: true, force: true })
+  }
+
   _updateBackground() {
     this._update().catch(noop)
   }
@@ -66,7 +84,8 @@ module.exports = class PearRuntime extends ReadyResource {
     this.updating = true
 
     const length = this.drive.core.length
-    const next = path.join(this.dir, 'pear-runtime', 'upgrade')
+    const id = length + '.' + this.drive.core.fork
+    const next = path.join(this.dir, 'pear-runtime/next', id)
     const co = this.drive.checkout(length)
 
     this.checkout = co
