e9d3e37e7e6fb41a1ab2bd56c60fd86d15163f44
Geordan Gesink <georgegeordan@gmail.com>
2026-02-17 03:35:56 +0100

    split rn and node/bare module

--- index.js
+++ index.js
@@ -1,108 +1,105 @@
-const { Worklet } = require('react-native-bare-kit') // NEED TO INSTALL IN ROOT PRJECT (version match)
-const bundle = require('./lib/pear.bundle.js')
-const RPC = require('bare-rpc')
-const RNFS = require('react-native-fs')
-const AsyncStorage = require('@react-native-async-storage/async-storage') // NEED TO INSTALL IN ROOT PRJECT (version match)
-const { DevSettings } = require('react-native') // NEED TO INSTALL IN ROOT PRJECT (version match)
-const b4a = require('b4a')
+/* global BareKit */
 
-module.exports = class PearMobile {
-  constructor(config = {}) {
-    if (!config.upgrade) throw new Error('upgrade link required')
-    this.config = {
-      dir: `${RNFS.DocumentDirectoryPath}/pear-runtime`,
-      isDev: __DEV__,
-      ...config
-    }
+const Hyperswarm = require('hyperswarm')
+const Hyperdrive = require('hyperdrive')
+const Corestore = require('corestore')
+const path = require('bare-path')
+const fs = require('bare-fs')
+const ReadyResource = require('ready-resource')
+const plink = require('pear-link')
+const hid = require('hypercore-id-encoding')
+const Localdrive = require('localdrive')
 
-    this._listeners = Object.create(null)
-    this._calledReady = null
+module.exports = class PearRuntime extends ReadyResource {
+  constructor(config) {
+    super()
+    if (!config.dir) throw new Error('PearRuntime needs "dir" to /Documents Sandbox')
+    const { drive: upgrade } = plink.parse(config.upgrade)
+    this.dir = config.dir
+    this.version = config.version || 0
+    this.app = config.app
+    this.name = config.name
+    this.key = hid.decode(upgrade.key)
+    this.length = upgrade.length || 0
+    this.fork = upgrade.fork || 0
+    this.link = plink.serialize({ drive: { fork: this.fork, length: this.length, key: this.key }})
+    this.store = config.store || new Corestore(path.join(this.dir, 'corestore'))
+    this.drive = new Hyperdrive(this.store, this.key)
+    this.swarm = config.swarm || null
+    this.next = null
+    this.checkout = null
 
-    this.IPCPromise = this.ready().catch(noop)
-  }
+    this.updating = false
+    this.updated = false
 
-  ready() {
-    if (this._calledReady) return this._calledReady
-    this._calledReady = this._open()
-    return this._calledReady
+    this.ready().catch(noop)
   }
 
   async _open() {
-    const runtimeDir = `${RNFS.DocumentDirectoryPath}/pear-runtime`
-    const storageDir = `${runtimeDir}/storage`
-
-    await RNFS.mkdir(runtimeDir)
-    await RNFS.mkdir(storageDir)
-
-    const argv = [JSON.stringify(this.config)]
-    const worklet = new Worklet()
-    worklet.start('/pear.bundle', bundle, argv)
-
-    return new RPC(worklet.IPC, async (req) => {
-      if (req.command === 0) {
-        const version = b4a.toString(req.data)
-        console.log('received version form pearend:', version)
-        this.emit('updated')
-      }
-      if (req.command === 1){
-        const updateData = b4a.toString(req.data)
-        console.log('[Update Diff]', updateData)
-      }
-      if (req.command === 2){
-        const logString = b4a.toString(req.data)
-        console.log('[pear-runtime]:', logString)
-      }
-    })
-  }
-
-  async applyUpdate(version) {
-    await AsyncStorage.multiSet([
-      ['updatePending', 'true'],
-      ['updateConfirmed', 'false'],
-      ['runtimeVersion', String(version ?? this.version)]
-    ])
-
-    DevSettings.reload()
-  }
-
-  async close() {}
-
-  on(event, callback) {
-    if (!this._listeners[event]) this._listeners[event] = []
-    this._listeners[event].push(callback)
-    return this
-  }
-
-  off(event, callback) {
-    if (!this._listeners[event]) return this
-    if (callback) {
-      this._listeners[event] =
-        this._listeners[event].filter(fn => fn !== callback)
-    } else {
-      this._listeners[event] = []
+    await this.drive.ready()
+    await fs.mkdir(path.join(this.dir, 'pear-runtime', 'upgrade'), { recursive: true, force: true })
+    if (!this.swarm) {
+      const keyPair = await this.store.createKeyPair('pear-container')
+      this.swarm = new Hyperswarm({ keyPair })
     }
-    return this
+
+    this.swarm.on('connection', (connection) => this.store.replicate(connection))
+    this.swarm.join(this.drive.core.discoveryKey, { client: true, server: false })
+
+    this._updateBackground()
+    this.drive.core.on('append', () => this._updateBackground())
   }
 
-  once(event, fn) {
-    const wrap = (...args) => {
-      this.off(event, wrap)
-      fn(...args)
+  async _close() {
+    await this.drive.close()
+    if (this.checkout) await this.checkout.close()
+    await this.store.destroy()
+    await this.swarm.destroy()
+  }
+
+  _updateBackground() {
+    this._update().catch(noop)
+  }
+
+  async _update() {
+    if (this.updating) return
+    this.updating = true
+
+    const length = this.drive.core.length
+    const next = path.join(this.dir, 'pear-runtime', 'upgrade')
+    const co = this.drive.checkout(length)
+
+    this.checkout = co
+
+    const manifest = await co.get('/package.json')
+    const version = manifest && JSON.parse(manifest).version
+    if (!manifest || version === this.version) {
+      this.updating = false
+      this.checkout = null
+      await co.close()
+      return
     }
-    return this.on(event, wrap)
-  }
 
-  emit(event, ...args) {
-    const list = this._listeners[event]
-    if (!list) return this
-    for (const fn of list) fn(...args)
-    return this
-  }
+    const local = new Localdrive(next)
 
-  run(filename, bundle, argv) {
-    const worklet = new Worklet()
-    worklet.start(filename, bundle, argv)
-    return worklet.IPC
+    this.emit('updating')
+
+    for await (const data of co.mirror(local)) {
+      this.emit('updating-delta', data)
+    }
+
+    await co.close()
+    await local.close()
+
+    this.checkout = null
+    this.length = length
+    this.next = next
+
+    this.updating = false
+    this.updated = true
+    this.emit('updated', version)
+
+    if (this.drive.core.length > length) this._updateBackground()
   }
 }
 
