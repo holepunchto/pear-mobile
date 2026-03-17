d43fa14fd321a558580628147a0a730a8e8c2b3f
Geordan Gesink <georgegeordan@gmail.com>
2026-03-17 17:22:31 +0100

    await swarm destroy; swarm -> this.swarm; store -> this.updater.store; typos

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const PearRuntimeUpdater = require('pear-runtime-updater')
-const ReadyResouce = require('ready-resource')
+const ReadyResource = require('ready-resource')
 const Corestore = require('corestore')
 const Hyperswarm = require('hyperswarm')
 const path = require('bare-path')
 const dir = require('bare-storage')
 const fs = require('bare-fs')
 
-module.exports = class PearRuntime extends ReadyResouce {
+module.exports = class PearRuntime extends ReadyResource {
   constructor(opts = {}) {
     super()
     if ((!opts.store && !!opts.swarm) || (!!opts.store && !opts.swarm)) {
       throw new Error('must pass store if passing swarm and vice versa')
     }
     if (!opts.dir) opts.dir = dir.persistent()
-    if (!opts.store) opts.store = new Corestore(path.join(this.dir, 'pear-runtime/corestore'))
+    if (!opts.store) opts.store = new Corestore(path.join(opts.dir, 'pear-runtime/corestore'))
 
     this.swarm = opts.swarm || null
     this.isModuleSwarm = this.swarm === null
@@ -30,18 +30,18 @@ module.exports = class PearRuntime extends ReadyResouce {
   async _open() {
     await this.updater.ready()
     if (this.swarm === null) {
-      const keyPair = await store.createKeyPair('pear-runtime')
+      const keyPair = await this.updater.store.createKeyPair('pear-runtime')
       this.swarm = new Hyperswarm({ keyPair, bootstrap: this.bootstrap })
     }
-    swarm.on('connection', (connection) => this.updater.store.replicate(connection))
-    swarm.join(this.updater.drive.core.discoveryKey, {
+    this.swarm.on('connection', (connection) => this.updater.store.replicate(connection))
+    this.swarm.join(this.updater.drive.core.discoveryKey, {
       client: true,
       server: false
     })
   }
 
   async _close() {
-    if (this.isModuleSwarm) this.swarm.destroy()
+    if (this.isModuleSwarm) await this.swarm.destroy()
     await this.updater.close()
   }
 }
