ec9897d3e9a07edb79ec4a4ed2cde41add8da058
Geordan Gesink <165498764+geordangesink@users.noreply.github.com>
2026-03-18 21:37:20 +0100

    move swarming outside updater module (#6)
    * move swarming outside updater module
    
    * fix names; store only on updater instance
    
    * await swarm destroy; swarm -> this.swarm; store -> this.updater.store; typos
    
    * close store if instanciated
    
    * readme
    
    * !! ->
    
    * replace isModuleSwarm and isModuleStore; use this.store
    
    * Apply suggestion from @davidmarkclements
    
    * Apply suggestion from @davidmarkclements
    
    * Apply suggestion from @davidmarkclements
    
    * Apply suggestion from @davidmarkclements
    
    * Apply suggestion from @davidmarkclements
    
    * Apply suggestion from @davidmarkclements
    
    * Apply suggestion from @davidmarkclements
    
    ---------
    
    Co-authored-by: David Mark Clements <david.mark.clements@gmail.com>

--- README.md
+++ README.md
@@ -51,10 +51,12 @@ Create a runtime. `opts` may include:
 - **`dir`** Directory to store data (e.g. app data dir). Defaults to `/Documents`.
 - **`upgrade`** – (required) Pear link for OTA updates (e.g. from `package.json` `upgrade` field).
 - **`name`** - (required) The package.json prductName of the app.
-- **`app`** – The path to the local OTA react-native bundle as booted from native code. (defaults to [pear-runtime-react-native](https://github.com/holepunchto/pear-runtime-react-native) default)
-- **`version`** – Current app version (e.g. from `package.json`); used for update checks.
-- **`updates`** – Set to `false` to disable P2P OTA updates.
-- **`storage`** – Saves the app storage path.
+- **`store`** - (optional) pass a [Corestore](https://github.com/holepunchto/corestore) to be used for updates. If passed `swarm` must also be passed. The `store` should be replicated over the `swarm`.
+- **`swarm`** - (optional) pass a [Hyperswarm](https://github.com/holepunchto/hyperswarm) to be used for swarming updates. If passed `store` must also be passed. The `store` should be replicated over the `swarm`.
+- **`app`** – (optional) The path to the local OTA react-native bundle as booted from native code. (defaults to [pear-runtime-react-native](https://github.com/holepunchto/pear-runtime-react-native) default)
+- **`version`** – (optional) Current app version (i.e. from `package.json`); used for update checks.
+- **`updates`** – (optional) Set to `false` to disable P2P OTA updates.
+- **`storage`** – (optional) Saves the app storage path.
 
 #### `pear.storage`
 
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const PearRuntimeUpdater = require('pear-runtime-updater')
-const ReadyResouce = require('ready-resource')
+const ReadyResource = require('ready-resource')
+const Corestore = require('corestore')
+const Hyperswarm = require('hyperswarm')
 const path = require('bare-path')
 const dir = require('bare-storage')
 const fs = require('bare-fs')
 
-module.exports = class PearRuntime extends ReadyResouce {
+module.exports = class PearRuntime extends ReadyResource {
   constructor(opts = {}) {
     super()
+    if ((!opts.store && opts.swarm) || (opts.store && !opts.swarm)) {
+      throw new Error('must pass store if passing swarm and vice versa')
+    }
     if (!opts.dir) opts.dir = dir.persistent()
+    this.opts = opts
+    if (!opts.store) opts.store = new Corestore(path.join(opts.dir, 'pear-runtime/corestore'))
+    this.store = opts.store
 
+    this.swarm = opts.swarm || null
+    this.bootstrap = opts.bootstrap
     this.dir = opts.dir
     this.storage = opts.storage || path.join(this.dir, 'app-storage')
 
@@ -20,9 +30,20 @@ module.exports = class PearRuntime extends ReadyResouce {
 
   async _open() {
     await this.updater.ready()
+    if (this.swarm === null) {
+      const keyPair = await this.store.createKeyPair('pear-runtime')
+      this.swarm = new Hyperswarm({ keyPair, bootstrap: this.bootstrap })
+      this.swarm.on('connection', (connection) => this.store.replicate(connection))
+      this.swarm.join(this.updater.drive.core.discoveryKey, {
+        client: true,
+        server: false
+      })
+    }
   }
 
   async _close() {
+    if (this.opts.swarm) await this.swarm.destroy()
     await this.updater.close()
+    if (this.opts.store) await this.store.close()
   }
 }
