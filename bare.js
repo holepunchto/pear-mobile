c00f4d065be7dec5a15b0004e5d52f43344ce2b8
Geordan Gesink <georgegeordan@gmail.com>
2026-08-14 00:21:50 +0200

    add native version management & OTA version skip

--- bare.js
+++ bare.js
@@ -2,9 +2,12 @@ const PearRuntimeUpdater = require('pear-runtime-updater')
 const ReadyResource = require('ready-resource')
 const Corestore = require('corestore')
 const Hyperswarm = require('hyperswarm')
+const semver = require('bare-semver')
+const { platform, arch } = require('which-runtime')
 const path = require('bare-path')
 const dir = require('bare-storage')
 const fs = require('bare-fs')
+const host = platform + '-' + arch
 
 module.exports = class PearRuntime extends ReadyResource {
   constructor(opts = {}) {
@@ -21,10 +24,16 @@ module.exports = class PearRuntime extends ReadyResource {
     this.bootstrap = opts.bootstrap
     this.dir = opts.dir
     this.storage = opts.storage || path.join(this.dir, 'app-storage')
+    this.skipUpdate = opts.skipUpdate || null
 
     const appPath = opts.app || path.join(opts.dir, 'pear-runtime', 'ota')
     if (!fs.existsSync(appPath)) fs.mkdirSync(appPath, { recursive: true, force: true })
-    this.updater = new PearRuntimeUpdater({ ...opts, app: appPath })
+    this.updater = new PearRuntimeUpdater({
+      ...opts,
+      app: appPath,
+      skipUpdate: () => this._skipUpdate()
+    })
+    this.updater.on('updated', () => this._writeManifest())
 
     this.ready().catch(noop)
   }
@@ -46,6 +55,24 @@ module.exports = class PearRuntime extends ReadyResource {
     if (this.opts.swarm) await this.swarm.destroy()
     await this.updater.close()
     if (this.opts.store) await this.store.close()
+  }
+
+  async _skipUpdate() {
+    if (this.skipUpdate && (await this.skipUpdate())) return true
+    await this.updater.drive.update()
+    const buffer = await this.updater.drive.get('/pear.json')
+    const minVer = buffer && JSON.parse(buffer).updates?.minVer
+    if (!minVer) return false
+    const current = semver.Version.parse(this.updater.version)
+    const skip = current.compare(semver.Version.parse(minVer)) < 0
+    if (skip) this.emit('update-incompatible', { minVer, version: this.updater.version })
+    return skip
+  }
+
+  _writeManifest() {
+    const buffer = this.updater.nextManifest
+    if (!buffer) return
+    fs.writeFileSync(path.join(this.updater.next, 'by-arch', host , 'app', this.updater.name, 'package.json'), buffer)
   }
 }
 
